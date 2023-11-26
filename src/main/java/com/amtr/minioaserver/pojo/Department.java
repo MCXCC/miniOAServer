0b0a0d0eeeeb9d1dcbdb40e765b5dfe4eb98483b
MCXCC <1270116382@qq.com>
2023-11-26 14:07:19 +0800

    bug修复

--- src/main/java/com/amtr/minioaserver/pojo/Department.java
+++ src/main/java/com/amtr/minioaserver/pojo/Department.java
@@ -1,7 +1,14 @@
 package com.amtr.minioaserver.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
     /**
      * id
